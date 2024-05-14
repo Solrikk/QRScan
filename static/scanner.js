98d9da10f769caa57eb2c1adb5eeae4de449ee06
Sviatoslav König <reveni324@gmail.com>
2024-05-14 14:17:18 +0300

    Update scanner.js

--- static/scanner.js
+++ static/scanner.js
@@ -3,47 +3,53 @@ function onScanSuccess(decodedText, decodedResult) {
     alert(`Код распознан = ${decodedText}`);
 }
 function onScanError(errorMessage) {
-    // обработать ошибку сканирования
 }
-var html5QrcodeScanner = new Html5QrcodeScanner("reader", { 
-    fps: 10, 
-    qrbox: 300,
-    rememberLastUsedCamera: true, 
-    aspectRatio: 1.777
+var html5QrcodeScanner = new Html5QrcodeScanner("reader", {
+    fps: 10,
+    qrbox: 250,
+    rememberLastUsedCamera: true
 });
 html5QrcodeScanner.render(onScanSuccess, onScanError);
+
 function ping(timeStart) {
     const timeEnd = performance.now();
-    const ping = timeEnd - timeStart;
-    document.getElementById('ping-value').innerText = `Пинг: ${Math.round(ping)} мс`;
+    document.getElementById('ping-value').innerText = `Пинг: ${Math.round(timeEnd - timeStart)} мс`;
+    setTimeout(() => ping(performance.now()), 2000);
 }
 function updateCameraDetails(details) {
     const detailsElement = document.getElementById('camera-details');
     detailsElement.className = "alert alert-info";
-    detailsElement.innerHTML = '<h4>Детали камеры:</h4>';
-    if(details) {
-        detailsElement.innerHTML += `<p>ID: ${details.id}</p>`;
-        detailsElement.innerHTML += `<p>Название: ${details.label}</p>`;
-        detailsElement.innerHTML += `<p>Разрешение: ${details.resolution.width} x ${details.resolution.height}</p>`;
-        detailsElement.innerHTML += '<p id="ping-value">Пинг: Вычисляется...</p>';
-        const timeStart = performance.now();
-        ping(timeStart); // Вызов функции ping для вычисления задержки
-    } else {
-        detailsElement.innerHTML += '<p>Детали недоступны</p>';
-    }
+    detailsElement.innerHTML = '<h4>Детали камеры:</h4>' + 
+        `<p>ID: ${details.id}</p>` + 
+        `<p>Название: ${details.label}</p>` + 
+        `<p>Разрешение: ${details.resolution.width} x ${details.resolution.height}</p>` + 
+        '<p id="ping-value">Пинг: Вычисляется...</p>';
+    ping(performance.now());
 }
-Html5Qrcode.getCameras().then(devices => {
-    if (devices && devices.length) {
-        const camera = devices[0];
-        updateCameraDetails({
-            id: camera.id,
-            label: camera.label,
-            resolution: { width: 'N/A', height: 'N/A' },
-        });
-    }
-}).catch(err => {
-    console.error("Ошибка получения камер:", err);
-    const detailsElement = document.getElementById('camera-details');
-    detailsElement.className = "alert alert-danger";
-    detailsElement.innerHTML = `<h4>Ошибка:</h4> <p>Запрашиваемое устройство не найдено. Пожалуйста, убедитесь, что к вашему устройству подключена камера и предоставлены разрешения на доступ к камере.</p>`;
-});
+
+function updateScreenSizeDetails() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const screenSizeElement = document.getElementById('screen-size');
+    screenSizeElement.innerText = `Размер экрана: ${width} x ${height}`;
+}
+
+window.onload = function() {
+    updateScreenSizeDetails();
+    Html5Qrcode.getCameras().then(devices => {
+        if (devices && devices.length) {
+            updateCameraDetails({
+                id: devices[0].id,
+                label: devices[0].label,
+                resolution: { width: 'N/A', height: 'N/A' }
+            });
+        }
+    }).catch(err => {
+        console.error("Ошибка получения камер:", err);
+        const detailsElement = document.getElementById('camera-details');
+        detailsElement.className = "alert alert-danger";
+        detailsElement.innerHTML = `<h4>Ошибка:</h4> <p>Запрашиваемое устройство не найдено. Пожалуйста, убедитесь, что к вашему устройству подключена камера и предоставлены разрешения на доступ к камере.</p>`;
+    });
+};
+
+window.onresize = updateScreenSizeDetails;
