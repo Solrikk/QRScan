ae83eb77281b2781a525ff36e0124e111e8f7557
Sviatoslav König <reveni324@gmail.com>
2024-05-14 15:13:26 +0300

    Update scanner.js

--- static/scanner.js
+++ static/scanner.js
@@ -1,6 +1,5 @@
 function onScanSuccess(decodedText, decodedResult) {
     document.getElementById('reader').style.backgroundColor = 'green';
-    
     setTimeout(() => document.getElementById('reader').style.backgroundColor = '', 3000);
 }
 function onScanError(errorMessage) {
@@ -12,26 +11,61 @@ var html5QrcodeScanner = new Html5QrcodeScanner("reader", {
     aspectRatio: 1.777
 });
 html5QrcodeScanner.render(onScanSuccess, onScanError);
+
 function ping(timeStart) {
     const timeEnd = performance.now();
     const ping = timeEnd - timeStart;
     document.getElementById('ping-value').innerText = `Пинг: ${Math.round(ping)} мс`;
     setTimeout(() => ping(performance.now()), 2000);
 }
+
+function updateMoscowTime() {
+    const moscowTimeOffset = 3;
+    const localTime = new Date();
+    const moscowTime = new Date(localTime.getTime() + moscowTimeOffset * 3600 * 1000);
+    document.getElementById('moscow-time').innerText = `МСК время: ${moscowTime.toISOString().split('T')[1].slice(0, 8)}`;
+    setTimeout(updateMoscowTime, 1000);
+}
+
+function saveScanResult(decodedText) {
+    fetch('/scan-qr/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ data: decodedText }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+function onScanSuccess(decodedText, decodedResult) {
+    document.getElementById('reader').style.backgroundColor = 'green';
+    setTimeout(() => document.getElementById('reader').style.backgroundColor = '', 3000);
+    saveScanResult(decodedText);
+}
+
 function updateCameraDetails(details) {
     const detailsElement = document.getElementById('camera-details');
     detailsElement.className = "alert alert-info";
-    detailsElement.innerHTML = '<h4>Детали камеры:</h4>';
+    detailsElement.innerHTML = '<h4>Детали камеры:</h4>' + '<p id="moscow-time">МСК время: Вычисляется...</p>';
     if(details) {
         detailsElement.innerHTML += `<p>ID: ${details.id}</p>`;
         detailsElement.innerHTML += `<p>Название: ${details.label}</p>`;
         detailsElement.innerHTML += `<p>Разрешение: ${details.resolution.width} x ${details.resolution.height}</p>`;
         detailsElement.innerHTML += '<p id="ping-value">Пинг: Вычисляется...</p>';
         ping(performance.now());
+        updateMoscowTime();
     } else {
         detailsElement.innerHTML += '<p>Детали недоступны</p>';
     }
 }
+
 Html5Qrcode.getCameras().then(devices => {
     if (devices && devices.length) {
         const camera = devices[0];
